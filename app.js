eefe245d5dc42ea227ab425ff98595c81ef6f8e2
nerdale <alexandra.neira@gmail.com>
2018-05-28 10:13:55 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,72 @@
+// OBJETO LITERAL
+
+const obj = {
+  key: 'value'
+}
+
+const obj = {
+  'key': 'value'
+}
+
+// DOS FORMAS DE ACCEDER A UN OBJETO
+
+key.value // dot notation
+
+key['value'] // corchetes
+
+// AGREGAR NUEVAS KEYS
+
+objeto.nuevaPropiedad = nuevoValor
+
+objeto['nuevaPropiedad'] = nuevoValor
+
+// ejemplo objeto literal con método
+
+const laboratorian = {
+  gender: 'female',
+  techSkills: ['JS', 'Git', 'Github', 'UX', 'HTML5', 'CSS3'],
+  // metodo
+  study: (condition, skills) => {
+    /*
+    console.log('Hola mi género es ' + laboratorian.gender + ' y me especializo en ' + laboratorian.techSkills[0]);
+    */
+
+    // template string
+    console.log(`Hola mi género es ${laboratorian.gender} y me especializo en ${laboratorian.techSkills[0]}`);
+  }
+}
+
+// accedo al metodo
+laboratorian.study()
+
+const bambi = {}
+
+bambi.nombre = 'Loreto';
+
+// CONSTRUCTOR
+
+class Person {
+  constructor (name, gender, age) {
+    this.name = name,
+    this.gender = gender,
+    this.age = age,
+    this.hobbies = []
+  }
+
+  // metodo: comportamiento del objeto
+  enjoy(pasatiempos) {
+    this.hobbies.push(pasatiempos)
+    return this.hobbies
+  }
+}
+
+let bambi = new Person('Loreto', 'Female', 28);
+let teresita = new Person('Teresita', 'Female', 36);
+bambi.enjoy('Leer');
+
+
+/*
+Crear un objeto constructor llamado rectangle que tenga las propiedades base y height
+El valor de estas propiedades son los parámetros ingresados
+Crea tres instancias y muestra el resultado del área
+*/
